47291879bf8c937a965e45a5d37bba40c2403e0a
Murata Mizuki <muratamizuki1024@gmail.com>
2026-04-24 11:49:35 +0900

    test3

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   const testVariable = "使わない変数";// これを追記して保存
   const test = "使わない変数";// これを追記して保存
+  const test2 = "使わない変数";// これを追記して保存
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
